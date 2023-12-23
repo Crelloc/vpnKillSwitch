7e1a28aeea017b1b06110d6524f661b5ce41525b
Thomas Turner <thomastdt@gmail.com>
2023-12-23 14:53:02 -0800

    load-wg-conf.sh: code refactoring user input
    Signed-off-by: Thomas Turner <thomastdt@gmail.com>

--- load-wg-conf.sh
+++ load-wg-conf.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+filenameExt=$(basename -- "$1")
+filename="${filenameExt%.*}"
+logfile=vks.log
+
 error() {
 	echo "synopsis: $0 [wg config name] [persistent after boot (optional): [y|n]]"
-        echo "example 1: $0 wg0 y"
-        echo "example 2: $0 wg0.conf n"
-        echo "example 2a: $0 wg0"
-        exit 1;
+	echo "example 1: $0 wg0 y"
+	echo "example 2: $0 wg0.conf n"
+	echo "example 2a: $0 wg0"
+	exit 1;
 }
 
 if [ "$EUID" -ne 0 ]; then
@@ -23,16 +27,11 @@ if [ -z "$1" ] || [[ ! -z "$2" && ( "$2" != [yY] && "$2" != [yY][eE][sS] && "$2"
 	error
 fi
 
-if [[ "$1" == *.conf ]]; then
-	if [ ! -f "/etc/wireguard/$1" ]; then
-        	echo "/etc/wireguard/$1 does not exist"
-        	exit 1
-	fi
-else
-	if [ ! -f "/etc/wireguard/$1.conf" ]; then
-                echo "/etc/wireguard/$1.conf does not exist"
-                exit 1
-        fi
+if [ ! -f "/etc/wireguard/$1.conf" ]; then
+	echo "/etc/wireguard/$1.conf does not exist"
+	echo "Available files:"
+	ls /etc/wireguard/*.conf
+    exit 1
 fi
 
 if [[ ! -z "$2" ]]; then
@@ -45,9 +44,6 @@ else
 	ENV_PERSIST=n
 fi
 
-filenameExt=$(basename -- "$1")
-filename="${filenameExt%.*}"
-logfile=vks.log
 
 for fpath in /etc/wireguard/*.conf; do
 	wg-quick down $fpath >> $logfile 2>&1
