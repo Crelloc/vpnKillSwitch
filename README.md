6bc8725ee7aba7594ad055b0b2391ae3282b2f1e
Thomas Turner <thomastdt@gmail.com>
2023-12-22 22:14:16 -0800

    added error handling and better user input
    Signed-off-by: Thomas Turner <thomastdt@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-ENV_PERSIST="y" sudo -E ./load-wg-conf.sh <<< /etc/wireguard/aws-lax0.conf
+## synopsis: $0 [wg config name] [persistent after boot (optional): [y|n]]
+## example 1: $0 wg0 y
+## example 2: $0 wg0.conf n
+## example 2a: $0 wg0
--- load-wg-conf.sh
+++ load-wg-conf.sh
@@ -1,17 +1,51 @@
 #!/bin/bash
 
-read -p "Enter path to wireguard conf: " path
-if [ ! -f "$path" ]; then
-	echo "file: $path doesn't exist"
-	exit 1
+error() {
+	echo "synopsis: $0 [wg config name] [persistent after boot (optional): [y|n]]"
+        echo "example 1: $0 wg0 y"
+        echo "example 2: $0 wg0.conf n"
+        echo "example 2a: $0 wg0"
+        exit 1;
+}
+
+if [ ! -z "$3" ]; then
+        echo "too many arguements"
+	error
 fi
 
-filenameExt=$(basename -- $path)
+
+if [ -z "$1" ] || [[ ! -z "$2" && ( "$2" != [yY] && "$2" != [yY][eE][sS] && "$2" != [nN] && "$2" != [nN][oO] ) ]]; then
+	error
+fi
+
+if [[ "$1" == *.conf ]]; then
+	if [ ! -f "/etc/wireguard/$1" ]; then
+        	echo "/etc/wireguard/$1 does not exist"
+        	exit 1
+	fi
+else
+	if [ ! -f "/etc/wireguard/$1.conf" ]; then
+                echo "/etc/wireguard/$1.conf does not exist"
+                exit 1
+        fi
+fi
+
+if [[ ! -z "$2" ]]; then
+	if [[ "$2" == [yY] || "$2" == [yY][eE][sS] ]]; then
+		ENV_PERSIST=y
+	elif [[ "$2" == [nN] || "$2" == [nN][oO]  ]]; then
+		ENV_PERSIST=n
+	fi
+else
+	ENV_PERSIST=n
+fi
+
+filenameExt=$(basename -- "$1")
 filename="${filenameExt%.*}"
-ENV_PERSIST=${ENV_PERSIST:-n}
+logfile=vks.log
 
 for fpath in /etc/wireguard/*.conf; do
-	wg-quick down $fpath
+	wg-quick down $fpath >> $logfile 2>&1
 	fExt=$(basename -- $fpath)
 	fName="${fExt%.*}"
 	if [[ $ENV_PERSIST == 'y' ]]; then
@@ -19,11 +53,9 @@ for fpath in /etc/wireguard/*.conf; do
 		echo "
        	 	systemctl disable wg-quick@$fName:
 		--------------------------------------------------------------------
-		"
-		systemctl stop "wg-quick@$fName"
-		systemctl disable "wg-quick@$fName"
-	#	systemctl status "wg-quick@$fName" | head -7
-	#	rm -i "/etc/systemd/system/wg-quick@$fName*"
+		" >> $logfile 2>&1
+		systemctl stop "wg-quick@$fName" >> $logfile 2>&1
+		systemctl disable "wg-quick@$fName" >> $logfile 2>&1
         	systemctl daemon-reload
 		systemctl reset-failed
 	fi
@@ -33,17 +65,12 @@ if [[ $ENV_PERSIST == 'y' ]]; then
 	echo "
 	systemctl enable wg-quick@$filename:
 	--------------------------------------------------------------------
-	"
-	systemctl enable "wg-quick@$filename"
-	systemctl daemon-reload
-	systemctl start "wg-quick@$filename"
-	#echo "
-	#systemctl status wg-quick@$filename:
-	#--------------------------------------------------------------------
-	#"
-	#systemctl status "wg-quick@$filename" | head -7
+	" >> $logfile 2>&1
+	systemctl enable "wg-quick@$filename" >> $logfile 2>&1
+	systemctl daemon-reload 
+	systemctl start "wg-quick@$filename" >> $logfile 2>&1
 else
-	wg-quick up "$path"
+	wg-quick up "$path" >> $logfile 2>&1
 fi
 
 if [[ $ENV_PERSIST == 'y' ]]; then
@@ -52,23 +79,20 @@ if [[ $ENV_PERSIST == 'y' ]]; then
 	echo "
 	systemctl environment variables:
 	--------------------------------------------------------------------
-	"
-	systemctl show-environment
+	" >> $logfile 2>&1
+	systemctl show-environment >> $logfile 2>&1
 
-	systemctl stop vpnKillSwitch.service
-	systemctl start vpnKillSwitch.service
+	systemctl stop vpnKillSwitch.service >> $logfile 2>&1
+	systemctl start vpnKillSwitch.service >> $logfile 2>&1
 	systemctl unset-environment tunnel
-	#echo "
-	#systemctl status vpnKillSwitch.service:
-	#"
-	#systemctl status vpnKillSwitch.service | head -7
-	/sbin/iptables -nL
+	/sbin/iptables -nL >> $logfile 2>&1
 else
 	echo "
 	non persistant firewall
 	--------------------------------------------------------------------
-	"
+	" >> $logfile 2>&1
+
 	tunnel="$filename"
-	exec /etc/vpnKillSwitch/firewall-reload.sh
+	exec /etc/vpnKillSwitch/firewall-reload.sh >> $logfile 2>&1
 fi
 
